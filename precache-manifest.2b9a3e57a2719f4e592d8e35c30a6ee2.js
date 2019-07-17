3ccc3fddfb01b13fcd7266e6ccd3d433bdb298a8
Gordon MacMaster <gmacmast@uvm.edu>
2019-07-16 23:15:42 -0700

    Updates

---
+++ precache-manifest.2b9a3e57a2719f4e592d8e35c30a6ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f692dd5018da4a433a28b99c6f5072b",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "69def8f49d5f9e9d3c2d",
+    "url": "/reactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "69def8f49d5f9e9d3c2d",
+    "url": "/reactApp/static/js/main.d324a63b.chunk.js"
+  },
+  {
+    "revision": "4f6bef0ec26ba2365171",
+    "url": "/reactApp/static/js/runtime~main.56b3556b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
